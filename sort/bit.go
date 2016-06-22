a59486900e216a06b513a8de125257cb71f8c276
zhaijian <zhaijian@mrocker.com>
2016-06-22 13:29:13 +0800

    change name

--- sort/bit.go
+++ sort/bit.go
@@ -2,30 +2,25 @@ package main
 
 import "fmt"
 
-type bitSet struct {
-	b []byte
+type BitField []byte
+
+func NewBitField(len int) BitField {
+	return BitField(make([]byte, len/8+1))
 }
 
-func NewBitSet(len int) *bitSet {
-	b := make([]byte, len/8+1)
-	return &bitSet{
-		b: b,
-	}
-}
-
-func (b *bitSet) Set(n int) {
+func (b BitField) Set(n int) {
 	start, offset := n/8, n%8
-	b.b[start] |= 1 << uint(offset)
+	b[start] |= 1 << uint(offset)
 }
 
-func (b *bitSet) Test(n int) bool {
+func (b BitField) Test(n int) bool {
 	start, offset := n/8, n%8
-	return (b.b[start] & (1 << uint(offset))) != 0
+	return (b[start] & (1 << uint(offset))) != 0
 }
 
 func main() {
 	arr := []int{23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-	bs := NewBitSet(23)
+	bs := NewBitField(23)
 	for i := 0; i < len(arr); i++ {
 		bs.Set(arr[i])
 	}
