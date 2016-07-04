874ed463c74267ce91290ab5605c22403f37f7ff
zhaijian <782271923@qq.com>
2016-07-04 23:11:24 +0800

    quick sort

--- sort/quick.go
+++ sort/quick.go
@@ -5,46 +5,42 @@ import (
 )
 
 func main() {
-
-	arr := []int{6, 4, 5, 2, 1}
-	quickSort(arr, 0, len(arr))
+	arr := []int{5,4,3,2,1,5}
+	quickSort(arr, 0, len(arr)-1)
 	for _, item := range arr {
 		fmt.Print(item, " ")
 	}
 }
 
-//5,4,3,2,1
 func quickSort(arr []int, start, end int) {
-
 	mid := getMid(arr, start, end)
 	if mid-start > 1 {
 		quickSort(arr, start, mid)
 	}
-
 	if end-(mid+1) > 1 {
 		quickSort(arr, mid+1, end)
 	}
 }
 
 func getMid(arr []int, start, end int) int {
-
 	sign := arr[start]
-
-	i := start + 1
-	j := end - 1
-
+	i := start
+	j := end
 	for i < j {
-		for arr[i] < sign && i < end {
-			i++
-		}
-		for arr[j] >= sign && j > 0 {
+		for i < j && arr[j] >= sign {
 			j--
 		}
-		if i != j {
+		for i < j && arr[i] <= sign {
+			i++
+		}
+		if i < j {
 			temp := arr[i]
 			arr[i] = arr[j]
 			arr[j] = temp
 		}
+	}
+	if i < j {
+
 	}
 	arr[start] = arr[i]
 	arr[i] = sign
